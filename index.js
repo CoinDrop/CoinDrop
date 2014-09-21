333eae7752097628e378c237344c6932794c104f
Brandon <justtopla@gmail.com>
2014-09-20 17:30:53 -0700

    test

--- index.js
+++ index.js
@@ -73,6 +73,15 @@ app.use(bodyParser.json()); // app.use(bodyParser.json({ type: 'application/vnd.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+
+
 app.get('/', function(req,res){
 	 Kitten.find(function (err, kittens) {
     if (err) return console.error(err);
