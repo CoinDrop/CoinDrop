c4bccac1d7d2a8372e97849c93e951d8dbea8e46
Brandon <justtopla@gmail.com>
2014-09-20 17:23:00 -0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,63 @@ var methodOverride = require('method-override');
 var cors = require('cors');
 var db = require('./app/config/db');
 var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI || db.url;
+
 mongoose.connect(connectionString);
+
+//mongoose TEST
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("acceptable");
+});
+
+var kittySchema = mongoose.Schema({
+    name: String,
+    message: String
+})
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name"
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema)
+
+
+var silence = new Kitten({ name: 'Silence' })
+console.log(silence.name) // 'Silence'
+
+
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak() // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  fluffy.speak();
+});
+
+
+  var getData = function(){
+    var result = [];
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    var results = [];
+    for(var i=0; i< kittens.length;i++)
+    {
+      results[i] = JSON.stringify(kittens[i].message);
+    console.log(results[i]);
+    }
+});
+};
+getData();
+  //mongoose test over
+
+
+
+
+
 
 var port = process.env.PORT || 8080;
 
