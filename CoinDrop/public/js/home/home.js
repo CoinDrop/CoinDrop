ff7ef27c02a82fd8f2bd0f6a3192678e77732602
loqi <loqi@cruzio.com>
2014-09-22 10:33:20 -0700

    (cleanup) removes MainCtrl.js

---
+++ CoinDrop/public/js/home/home.js
@@ -0,0 +1,13 @@
+;(function(){
+  'use strict';
+  angular.module('coindropApp')
+  .config(['$stateProvider', configuration]);
+
+  function configuration($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'js/home/home.html'
+      })
+    }
+}).call(this);
