06bfa164dae28bdc4c6de2a4c0c8b095798c2d15
loqi <loqi@cruzio.com>
2014-09-24 09:21:29 -0700

    (test) creates a first test to test the test

--- specs/app/routesSpec.js
+++ specs/app/routesSpec.js
@@ -1,24 +1,11 @@
-var request = require('supertest');
-var express = require('express');
-var expect = require('chai').expect;
-var app = require('../../server.js');
+describe('Routing', function() {
 
-//var db = require('../app/config');
-//var User = require('../app/models/user');
-//var Link = require('../app/models/link');
-
-/////////////////////////////////////////////////////
-// NOTE: these tests are designed for mongo!
-/////////////////////////////////////////////////////
-
-  describe('Routing', function() {
-
-    // it('should work', function(done) {
-    //   request(app)
-    //     .get('/')
-    //     .expect(200)
-    //     .end(done);
-    // });
+  // it('should work', function(done) {
+  //   request(app)
+  //     .get('/')
+  //     .expect(200)
+  //     .end(done);
+  // });
 
   var $route;
 
