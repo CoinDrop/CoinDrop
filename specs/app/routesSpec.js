fd06a2533a1405d6be917c96e25239f1ce895dd7
loqi <loqi@cruzio.com>
2014-09-24 10:53:22 -0700

    (fix) moves specs/app/routeSpec.js to specs/public/routingSpec.js

--- specs/app/routesSpec.js
+++ specs/app/routesSpec.js
@@ -2,27 +2,26 @@ var request = require('supertest');
 var express = require('express');
 var expect = require('chai').expect;
 var app = require('../../server.js');
-  
-describe('Routing', function() {
 
-  // it('should work', function(done) {
-  //   request(app)
-  //     .get('/')
-  //     .expect(200)
-  //     .end(done);
-  // });
+//var db = require('../app/config');
+//var User = require('../app/models/user');
+//var Link = require('../app/models/link');
 
-  var $route;
+/////////////////////////////////////////////////////
+// NOTE: these tests are designed for mongo!
+/////////////////////////////////////////////////////
 
-  beforeEach(module('coindropApp'));
-  beforeEach(inject(function($injector){
-    $route = $injector.get('$route');
-  }));
+describe('', function() {
 
-  it('Should have /initiate route, template, and controller', function () {
-    expect($route.routes['/initiate']).to.be.ok();
-    expect($route.routes['/initiate'].controller).to.be('initiateCtrl');
-    expect($route.routes['/initiate'].templateUrl).to.be('scripts/states/initiate/initiate.html');
-  });
+  describe('Routing', function() {
 
-});
+    it('should work', function(done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(done);
+    });
+
+    });
+
+}); 
---
+++ specs/public/routingSpec.js
@@ -0,0 +1,32 @@
+var request = require('supertest');
+var express = require('express');
+var expect = require('chai').expect;
+var app = require('../../server.js');
+
+//var db = require('../app/config');
+//var User = require('../app/models/user');
+//var Link = require('../app/models/link');
+
+describe('Routing', function() {
+
+  // it('should work', function(done) {
+  //   request(app)
+  //     .get('/')
+  //     .expect(200)
+  //     .end(done);
+  // });
+
+  var $route;
+
+  beforeEach(module('coindropApp'));
+  beforeEach(inject(function($injector){
+    $route = $injector.get('$route');
+  }));
+
+  it('Should have /initiate route, template, and controller', function () {
+    expect($route.routes['/initiate']).to.be.ok();
+    expect($route.routes['/initiate'].controller).to.be('initiateCtrl');
+    expect($route.routes['/initiate'].templateUrl).to.be('scripts/states/initiate/initiate.html');
+  });
+
+});
