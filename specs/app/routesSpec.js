daa967e538a0032cd017e0ce86ffef6eafdd8521
loqi <loqi@cruzio.com>
2014-09-23 21:10:51 -0700

    (test) adds initial test for routeSpec.js

--- specs/app/routesSpec.js
+++ specs/app/routesSpec.js
@@ -11,17 +11,26 @@ var app = require('../../server.js');
 // NOTE: these tests are designed for mongo!
 /////////////////////////////////////////////////////
 
-describe('', function() {
-
   describe('Routing', function() {
 
-    it('should work', function(done) {
-      request(app)
-        .get('/')
-        .expect(200)
-        .end(done);
-    });
+    // it('should work', function(done) {
+    //   request(app)
+    //     .get('/')
+    //     .expect(200)
+    //     .end(done);
+    // });
 
-    });
+  var $route;
 
-}); 
+  beforeEach(module('coindropApp'));
+  beforeEach(inject(function($injector){
+    $route = $injector.get('$route');
+  }));
+
+  it('Should have /initiate route, template, and controller', function () {
+    expect($route.routes['/initiate']).to.be.ok();
+    expect($route.routes['/initiate'].controller).to.be('initiateCtrl');
+    expect($route.routes['/initiate'].templateUrl).to.be('scripts/states/initiate/initiate.html');
+  });
+
+});
