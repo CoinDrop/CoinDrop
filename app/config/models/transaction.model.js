700e6485e74801c81016f2237ecc5b648f5b2e37
gitSauced <ttan08@gmail.com>
2014-09-26 16:24:50 -0700

    (edit) removes comments

--- app/config/models/transaction.model.js
+++ app/config/models/transaction.model.js
@@ -17,39 +17,4 @@ var TransactionSchema = new Schema({
   }
 });
 
-// UserSchema.methods.comparePasswords = function (userPassword) {
-//   var defer = Q.defer();
-//   var savedPassword = this.password;
-//   bcrypt.compare(userPassword, savedPassword, function (err, isMatch) {
-//     if(err) {
-//       defer.reject(err);
-//     } else {
-//       defer.resolve(isMatch);
-//     }
-//   });
-//   return defer.promise;
-// };
-
-// UserSchema.pre('save', function (next) {
-//     var user = this;
-
-//     if(!user.isModified('password')) {
-//       return next();
-//     }
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//       if(err) {
-//         return next(err);
-//       }
-
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//       if(err) {
-//         return next(err);
-//       }
-//       user.password = hash;
-//       user.salt = salt;
-//       next();
-//     });
-//   });
-// });
-
 module.exports = mongoose.model('Transaction', TransactionSchema);
