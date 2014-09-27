b2ead41b534b2465d3adf13e8b7c4e190f95f561
gitSauced <ttan08@gmail.com>
2014-09-27 11:58:27 -0700

    (edit) adds home script to index

--- public/scripts/states/makeTran/makeTran.controller.js
+++ public/scripts/states/makeTran/makeTran.controller.js
@@ -4,11 +4,14 @@
     .module('coindropApp')
     .controller('MakeTranController', MakeTranController);
     /* @inject */
-    function MakeTranController($scope, MakeTranService){
+    function MakeTranController($scope, MakeTranService, $state){
       $scope.makeTran = {};
       $scope.doMakeTran = function(){
-        console.log($scope.makeTran);
         MakeTranService.doMakeTran($scope.makeTran);
+        $state.go('user');
+      };
+      $scope.returnToDash = function() {
+        $state.go('user');
       };
     }
 }).call(this);
