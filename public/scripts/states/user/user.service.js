eeac79e69200ad65b98e427b69ed4fe9e940022f
gitSauced <ttan08@gmail.com>
2014-09-27 12:06:09 -0700

    (edit) service can now grab the id of each individual transaction clicked

--- public/scripts/states/user/user.service.js
+++ public/scripts/states/user/user.service.js
@@ -4,15 +4,18 @@
     .module('coindropApp')
     .factory('userService', userService);
     /* @inject */
-    function userService (){
+    function userService ($http){
+
       return {
         chooseThisTrans: chooseThisTrans,
-        getAllTransactions: getAllTransactions
+        getAllTransactions: getAllTransactions,
+        getOneTrans: getOneTrans,
+        findData: findData
       };
       function chooseThisTrans (transaction) {
         return $http({
           method: 'GET',
-          url: '/transactions'
+          url: 'api/transactions'
         })
         .then(function (resp) {
           return resp;
@@ -22,8 +25,25 @@
       function getAllTransactions () {
         return $http({
           method: 'GET',
-          url: '/transactions'
+          url: 'api/transactions'
+        })
+        .then(function(resp) {
+          console.log('INSIDE GET ALL TRANS');
+          return resp;
         });
+      }
+      function getOneTrans(transactionId) {        
+        return $http({
+          method: 'GET',
+          url: 'api/transactions/'+transactionId
+        })
+        .then(function (resp) {
+          return resp;
+        });
+      }
+
+      function findData(id){
+        return _.find(data, {_id: id});
       }
 
     }
