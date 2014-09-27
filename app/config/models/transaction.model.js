2ca4783b6cc51a1191aeafdaccc82f3ab766f7c9
gitSauced <ttan08@gmail.com>
2014-09-27 11:50:12 -0700

    (edit) edits the transaction model to include wallet

--- app/config/models/transaction.model.js
+++ app/config/models/transaction.model.js
@@ -3,21 +3,25 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
 var Q = require('q');
 
+
 var TransactionSchema = new Schema({
-  memo: {
-    type: String,
-  },
+  memo: String,
   otherUsername: {
     type: String,
     required: true
+  },
+  me: {
+    type: String,
+    require: true
   },
   btc: {
     type: String,
     required: true
   },
-  greeting: {
-    type: String
-  }
+  greeting: String,
+  address: String,
+  key1: String,
+  key2: String
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
