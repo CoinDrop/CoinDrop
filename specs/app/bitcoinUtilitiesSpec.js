a1072cffc794798c102837035375c1ffc8753c04
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-02 15:54:14 -0700

    (test) adds tests for multi sig wallet creation function in bitcoinUtilities

--- specs/app/bitcoinUtilitiesSpec.js
+++ specs/app/bitcoinUtilitiesSpec.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var expect = require('chai').expect;
 var base58check = require(path.resolve(__dirname, '..', '..', 'node_modules/bitcoinjs-lib/node_modules/bs58check/src/bs58check.js'));
+var Bitcoin = require('bitcoinjs-lib');
 
 var Utilities = require(path.resolve(__dirname, '..', '..', 'app/bitcoinUtilities.js'));
 
@@ -18,36 +19,76 @@ describe('Utilities', function(){
 
 
   describe('makeWallet()', function(){
+    var wallet;
     beforeEach(function(){
-      wallet = Utilities.makeWallet();
+      wallet = Utilities.makeWallet(2, 3);
     });
 
     it('should return an object with correct properties', function(){
       expect(wallet).to.be.a('object');
       expect(wallet.address).to.be.ok;
-      expect(wallet.privateKey1).to.be.ok;
-      expect(wallet.privateKey2).to.be.ok;
+      expect(wallet.privateKeys).to.be.ok;
+      expect(wallet.publicHexes).to.be.ok;
+      expect(wallet.n).to.be.ok;
     });
 
     describe('.address', function(){
 
-      it('should be a valid BTC testnet address', function(){
-         expect(wallet.address).to.be.a('string');
-         expect(wallet.address.length).to.equal(34);
-         expect(base58check.decode(wallet.address)).to.be.ok;
-         expect(wallet.address[0]).to.match(/m|n/);
+      it('should be a valid multisig testnet address (testnet script hash)', function(){
+        expect(wallet.address).to.be.a('string');
+        expect(wallet.address.length).to.equal(35);
+        expect(base58check.decode(wallet.address)).to.be.ok;
+        expect(wallet.address[0]).to.equal('2');
       });
 
     });
 
-    describe('privatekeys', function(){
+    describe('.privatekeys', function(){
 
-      it('should be a valid BTC testnet privateKey, constructed from 2 properties', function(){
-        var privateKey = wallet.privateKey1 + wallet.privateKey2;
-        expect(privateKey).to.be.a('string');
-        expect(privateKey.length).to.equal(52);
-        expect(base58check.decode(privateKey)).to.be.ok;
-        expect(privateKey[0]).to.match(/K|L/);
+      it('should be a valid multisig testnet privateKey (for compressed pubkey)', function(){
+        expect(wallet.privateKeys).to.be.an('Array');
+        expect(wallet.privateKeys.length).to.equal(3);
+        wallet.privateKeys.forEach(function(privateKey){
+          expect(privateKey).to.be.a('string');
+          expect(privateKey.length).to.equal(52);
+          expect(base58check.decode(privateKey)).to.be.ok;
+          expect(privateKey[0]).to.equal('c');
+        });
+      });
+
+    });
+
+    describe('.publicHexes', function(){
+
+      it('should be an array with proper length', function(){
+        expect(wallet.publicHexes).to.be.an('Array');
+        expect(wallet.publicHexes.length).to.equal(3);
+      })
+
+      it('should contain hex strings that correspond to the wallet address', function(){
+        wallet.publicHexes.forEach(function(hex){
+          expect(hex.length).to.equal(66);
+          expect(parseInt(hex, 16)).to.be.ok;
+        });
+        var publicKeys = wallet.publicHexes.map(function(hex){
+          return new Bitcoin.ECPubKey.fromHex(hex);
+        });
+
+        var redeemScript = Bitcoin.scripts.multisigOutput(wallet.n, publicKeys);
+        var scriptPublicKey = Bitcoin.scripts.scriptHashOutput(redeemScript.getHash());
+        var address = Bitcoin.Address.fromOutputScript(scriptPublicKey, Bitcoin.networks.testnet).toString();
+
+        expect(wallet.address).to.equal(address);
+
+      });
+
+    });
+
+    describe('.n', function(){
+      
+      it('should be number of unique signatures needed for a valid transaction', function(){
+        expect(wallet.n).to.be.a('number');
+        expect(wallet.n).to.be.below(wallet.privateKeys.length);
       });
 
     });
