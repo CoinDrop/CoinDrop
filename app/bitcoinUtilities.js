bb8c5aa640d3ed0aca42b2d4aaedbf393c59b111
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-02 19:27:23 -0700

    (feat) adds withdraw function on bitcoin utilities to empty wallet

--- app/bitcoinUtilities.js
+++ app/bitcoinUtilities.js
@@ -1,12 +1,11 @@
 // Utilities for bitcoin logic stored here
 var Bitcoin = require('bitcoinjs-lib');
+var helloblock = require('helloblock-js')({
+  network: 'testnet',
+  debug: true
+});
 
 module.exports.makeWallet = function(n, m){
-
-  // create m private keys
-  // store m private keys and pub key hexes
-  // redeem script from n
-  // use redeem script for building wallet
 
   var privateKeys = [];
 
@@ -27,4 +26,55 @@ module.exports.makeWallet = function(n, m){
   }
 
   return wallet;
+};
+
+module.exports.withdraw = function(n, userKeys, publicHexes, destination, amount, fee){
+
+  fee = fee || 10000;
+  var publicKeys = publicHexes.map(function(hex){ return new Bitcoin.ECPubKey.fromHex(hex) });
+  var privateKeys = userKeys.map(function(key){ return new Bitcoin.ECKey.fromWIF(key) });
+
+  var redeemScript = Bitcoin.scripts.multisigOutput(n, publicKeys);
+  var scriptPublicKey = Bitcoin.scripts.scriptHashOutput(redeemScript.getHash());
+
+  var address = Bitcoin.Address.fromOutputScript(scriptPublicKey, Bitcoin.networks.testnet).toString();
+
+  helloblock.addresses.getUnspents(address, function(error, response, unspents){
+
+    if( !error && unspents.length ){
+      var txb = new Bitcoin.TransactionBuilder();
+      var totalUnspentsValue = 0;
+
+      unspents.forEach(function(unspent){
+        txb.addInput(unspent.txHash, unspent.index);
+        totalUnspentsValue += unspent.value;
+      });
+
+      txb.addOutput(destination, amount);
+      txb.addOutput(address, totalUnspentsValue - amount - fee);
+
+      txb.tx.ins.forEach(function(input, index){
+        for( var i = 0; i < n; i ++ ){
+          txb.sign(index, privateKeys[i], redeemScript);
+        }
+      });
+
+      console.log('inside withdraw - helloblock get unspents -redeemScript\t', redeemScript);
+
+      var tx = txb.build();
+      var hash = tx.toHex();
+
+      helloblock.transactions.propagate(hash, function(error, response, transaction){
+        if( !error ){
+          console.log('tx propagated!');
+          console.log('\nreponse:\t', response);
+        }
+      });
+
+    }
+
+  });
+
+
+
 };
