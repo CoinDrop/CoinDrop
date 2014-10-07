5fcb3407d1caf2cdc72369e7647f2fd56850ac1b
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-07 15:17:05 -0700

    (feat) adds route for withdrawing bitcoin

--- app/routes.js
+++ app/routes.js
@@ -244,7 +244,19 @@ module.exports = function(app) {
       });
     });
 
+  router.route('/withdraw')
+    .post(function(req, res){
+      console.log('withdrawing from wallet');
+      var n = req.n;
+      var enteredKeys = req.privateKeys;
+      var publicHexes = req.publicHexes;
+      var destination = req.destination;
+      var amount = req.amount;
+      var fee = req.fee || 10000;
 
+      BTCUtilities.withdraw(res, n, enteredKeys, publicHexes, destination, amount, fee);
+
+    });
 
   router.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
