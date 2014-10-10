d95f266daed632a55d7afb0b056ac6136a5de144
gitSauced <ttan08@gmail.com>
2014-10-10 16:24:22 -0700

    (feat) withdraw feature now all on the deal controller/service

--- public/scripts/states/deal/deal.controller.js
+++ public/scripts/states/deal/deal.controller.js
@@ -6,9 +6,25 @@
     
     /* @inject */
     function DealController($scope, dealService, $stateParams, $state, $storage) {
+      $scope.showform = false;
+      $scope.showbutton = true;
       var dealInfo = $stateParams.dealId;
       var userId = $storage.getObject('current_user');
       $scope.deal = dealInfo;
+      $scope.destination = {};
+
+      $scope.withdrawView = function() {
+        $scope.showbutton = false;
+        $scope.showform = true;
+      };
+
+      $scope.withdraw = function(){
+          dealService.withdraw(dealInfo, $scope.destination.address)
+          .then(function(res) {
+          $state.go('user');
+          $('#myModal').modal('hide');
+          });
+      };
 
       $scope.releaseKey = function () {
         dealService.releaseKey(dealInfo._id, userId._id);
